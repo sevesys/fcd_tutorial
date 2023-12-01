fab855308986936bab915650795b97bb64aedb7f
Jeffrey Clarke <sevesys@live.com>
2023-11-30 23:13:33 -0500

    adds billing

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,6 +86,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "fcd.users",
+    "fcd.billing",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
